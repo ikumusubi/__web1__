52f586a2d4f52433240f929051221ab80c748a14
ikumusubi <ikumusubidown@gmail.com>
2023-05-08 11:32:20 +0800

    modify

--- C01/ts/add-content.js
+++ C01/ts/add-content.js
@@ -1,6 +1,17 @@
 "use strict";
-let my_name = "空";
-console.log(my_name);
-let first_name = "ikusu";
-console.log(first_name);
-123456;
+let today = new Date();
+let hourNow = today.getHours();
+let greeting;
+if (hourNow > 18) {
+    greeting = "good morning!";
+}
+else if (hourNow > 12) {
+    greeting = "good afrernoon";
+}
+else if (hourNow > 0) {
+    greeting = "good morning";
+}
+else {
+    greeting = "Welcome!";
+}
+document.write('<h3>' + greeting + '</h3>');
